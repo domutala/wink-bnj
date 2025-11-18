a2468941dd23f98fda43ea4de431a810f389cd37
Mamadou Talla DIA <ibntalla@gmail.com>
2025-11-18 04:36:36 +0000

    fix: correction de la redirection vers la page d'inscription dans le middleware d'authentification

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,6 +2,6 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const { $localePath } = useNuxtApp();
 
   if (!Store.auth.auth && !to.path.startsWith("/auth")) {
-    return navigateTo($localePath({ name: "auth-login" }));
+    return navigateTo($localePath({ name: "auth-register" }));
   }
 });
